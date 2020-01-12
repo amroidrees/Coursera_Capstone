da76eb12c372750a159c1586f50b10cc2673b7b5
amroidrees <59786004+amroidrees@users.noreply.github.com>
2020-01-12 14:59:16 +0400

    commit week3

---
+++ week3.py
@@ -0,0 +1,82 @@
+import requests
+website_url = requests.get('https://en.wikipedia.org/wiki/List_of_postal_codes_of_Canada:_M').text
+from bs4 import BeautifulSoup
+soup = BeautifulSoup(website_url,'lxml')
+print(soup.prettify())
+My_table = soup.find('table',{'class':'wikitable sortable'})
+My_table
+print(My_table.tr.text)
+headers="Postcode,Borough,Neighbourhood"
+table1=""
+for tr in My_table.find_all('tr'):
+    row1=""
+    for tds in tr.find_all('td'):
+        row1=row1+","+tds.text
+    table1=table1+row1[1:]
+print(table1)
+file=open("toronto.csv","wb")
+#file.write(bytes(headers,encoding="ascii",errors="ignore"))
+file.write(bytes(table1,encoding="ascii",errors="ignore"))
+import pandas as pd
+df = pd.read_csv('toronto.csv',header=None)
+df.columns=["Postalcode","Borough","Neighbourhood"]
+df.head(10)
+# Get names of indexes for which column Borough has value "Not assigned"
+indexNames = df[ df['Borough'] =='Not assigned'].index
+
+# Delete these row indexes from dataFrame
+df.drop(indexNames , inplace=True)
+df.head(10)
+df.loc[df['Neighbourhood'] =='Not assigned' , 'Neighbourhood'] = df['Borough']
+df.head(10)
+result = df.groupby(['Postalcode','Borough'], sort=False).agg( ', '.join)
+df_new=result.reset_index()
+df_new.head(15)
+df_new.shape
+!wget -q -O 'Toronto_long_lat_data.csv'  http://cocl.us/Geospatial_data
+df_lon_lat = pd.read_csv('Toronto_long_lat_data.csv')
+df_lon_lat.head()
+df_lon_lat.columns=['Postalcode','Latitude','Longitude']
+df_lon_lat.head()
+Toronto_df = pd.merge(df_new,
+                 df_lon_lat[['Postalcode','Latitude', 'Longitude']],
+                 on='Postalcode')
+Toronto_df
+from geopy.geocoders import Nominatim # convert an address into latitude and longitude values
+
+# Matplotlib and associated plotting modules
+import matplotlib.cm as cm
+import matplotlib.colors as colors
+
+# import k-means from clustering stage
+from sklearn.cluster import KMeans
+
+#!conda install -c conda-forge folium=0.5.0 --yes # uncomment this line if you haven't completed the Foursquare API lab
+import folium # map rendering library
+
+print('Libraries imported.')
+address = 'Toronto, ON'
+
+geolocator = Nominatim(user_agent="Toronto")
+location = geolocator.geocode(address)
+latitude_toronto = location.latitude
+longitude_toronto = location.longitude
+print('The geograpical coordinate of Toronto are {}, {}.'.format(latitude_toronto, longitude_toronto))
+
+map_toronto = folium.Map(location=[latitude_toronto, longitude_toronto], zoom_start=10)
+
+# add markers to map
+for lat, lng, borough, Neighbourhood in zip(Toronto_df['Latitude'], Toronto_df['Longitude'], Toronto_df['Borough'], Toronto_df['Neighbourhood']):
+    label = '{}, {}'.format(Neighbourhood, borough)
+    label = folium.Popup(label, parse_html=True)
+    folium.CircleMarker(
+        [lat, lng],
+        radius=5,
+        popup=label,
+        color='blue',
+        fill=True,
+        fill_color='#3186cc',
+        fill_opacity=0.7,
+        parse_html=False).add_to(map_toronto)  
+    
+map_toronto
